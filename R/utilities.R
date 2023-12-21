8d0f152c3b35f59e439821ce0dd6df55bbf33c5b
LiNk-NY <marcel.ramos@roswellpark.org>
2023-12-21 15:48:06 -0500

    rename to compareBarcodes and document

--- R/utilities.R
+++ R/utilities.R
@@ -1,15 +1,70 @@
-bcInFiltered <- function(
-    raw_filt_resources, spacerangerOut, processing = c("raw", "filtered"), outs = "outs", barcodes
+.compare_list_bcodes <- function(bcode_list) {
+    inRes <- do.call(
+        `%in%`, unname(lapply(bcode_list, unlist, use.names = FALSE))
+    )
+    comp1 <- names(bcode_list)[1L]
+    res_frame <- cbind.data.frame(bcode_list[[comp1]], inRes)
+    colname1 <- paste0(comp1, "_", "barcodes")
+    colname2 <- paste0("in_", names(bcode_list)[2L])
+    names(res_frame) <- c(colname1, colname2)
+    res_frame
+}
+
+#' Compare barcodes between raw and filtered data
+#'
+#' @description This function compares the barcodes between raw and filtered
+#'  data **depending** on the order of `processing`.
+#'
+#' @param raw_filt_resources `character(2)` A vector of length 2, where the
+#'   first element is the path to the raw resources and the second element is
+#'   the path to the filtered resources. If the resources are already converted
+#'   to `TENxFileList` objects, then this argument can be a list of length 2,
+#'   where the first element is the raw resources and the second element is the
+#'   filtered resources.
+#'
+#' @param spacerangerOut `character(1)` A scalar vector specifying the path
+#'   to the `spaceranger` output directory.
+#'
+#' @param processing `character(2)` A vector of length 2 that corresponds to the
+#'   processing type. The processing types are typically "raw" and "filtered".
+#'   These are the prefixes of the folder names `raw_feature_bc_matrix` and
+#'   `filtered_feature_bc_matrix`.
+#'
+#' @param outs `character(1)` A single string specifying the name of the
+#'   `spaceranger` output directory. Default is "outs".
+#'
+#' @return A `data.frame` with barcodes of the first element in the `processing`
+#'   data type as the first column and a logical vector indicating whether the
+#'   barcodes are found in the second element in `processing`. For example,
+#'   if processing is `c("raw", "filtered")`, then the first column will be the
+#'   barcodes in the `raw` data and the second column will be a logical vector
+#'   indicating whether the barcodes are found in the `filtered` data.
+#'
+#' @examples
+#' if (interactive()) {
+#'     compareBarcodes(
+#'         raw_filt_resources = c(
+#'             "~/data/V1_Adult_Mouse_Brain_raw_feature_bc_matrix.tar.gz",
+#'             "~/data/V1_Adult_Mouse_Brain_filtered_feature_bc_matrix.tar.gz"
+#'         ),
+#'         processing = c("raw", "filtered"),
+#'         outs = "outs"
+#'     )
+#' }
+#' @export
+compareBarcodes <- function(
+    raw_filt_resources, spacerangerOut,
+    processing = c("raw", "filtered"), outs = "outs", ...
 ) {
-    if (!identical(processing, c("raw", "filtered")))
-        stop("'processing' types must be 'raw' and 'filtered' for comparison")
+    if (!identical(length(processing), 2L))
+        stop("Length of 'processing' must be 2, e.g., c('raw', 'filtered')")
     res <- structure(vector("list", 2L), .Names = processing)
     if (!missing(spacerangerOut)) {
         if (isScalarCharacter(spacerangerOut))
             stopifnot(dir.exists(spacerangerOut))
-        for (process in processin) {
+        for (process in processing) {
             res[[process]] <-
-                .find_convert_resources(spacerangerOut, processing, ...)
+                .find_convert_resources(spacerangerOut, process, ...)
         }
     } else {
         if (
@@ -17,6 +72,13 @@ bcInFiltered <- function(
             isCharacter(raw_filt_resources)
         )
             stop("'raw_filt_resources' must be a character vector of length 2")
+        keywordInFile <- mapply(
+            function(file, proc) {
+                grepl(pattern = proc, x = file, fixed = TRUE)
+            }, raw_filt_resources, processing
+        )
+        if (!all(keywordInFile))
+            stop("No 'processing' match in 'raw_filt_resources' file names")
         for (process in processing) {
             file <- grep(process, raw_filt_resources, value = TRUE)
             txfl <- TENxFileList(file)
@@ -33,7 +95,5 @@ bcInFiltered <- function(
         }
     )
     bcode_frames <- lapply(bcode_files, import)
-    do.call(
-        `%in%`, unname(lapply(bcode_frames, unlist))
-    )
+    .compare_list_bcodes(bcode_frames)
 }
