308f8591fa8adba07ebe1d31d2edc89a3b1ddaf8
LiNk-NY <marcel.ramos@roswellpark.org>
2024-09-11 14:25:31 +0200

    odir found only if path is character value pointing to folder

--- R/TENxVisium-class.R
+++ R/TENxVisium-class.R
@@ -81,9 +81,9 @@ setClassUnion("TENxFileList_OR_TENxH5", members = c("TENxFileList", "TENxH5"))
 
 #' @importFrom TENxIO TENxFileList
 .find_convert_resources <- function(path, processing, format, ...) {
+    if (!is(path, "TENxFileList")) {
         odir <- list.dirs(path, recursive = FALSE, full.names = TRUE)
         stopifnot("The 'outs' directory was not found." = endsWith(odir, "outs"))
-    if (!is(path, "TENxFileList")) {
         path <- .find_file_or_dir(odir, processing, format)
     } else {
         if (identical(format, "mtx"))
