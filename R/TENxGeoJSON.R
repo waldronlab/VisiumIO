3e9899582626db89e17f288fe91a64f263e812a6
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-10-03 15:22:32 -0400

    document TENxGeoJSON class

--- R/TENxGeoJSON.R
+++ R/TENxGeoJSON.R
@@ -1,9 +1,27 @@
+#' @title Import 10X Genomics GeoJSON files
+#'
+#' @description `TENxGeoJSON` is a class to represent and import GeoJSON files
+#'   from 10X Genomics. It is a composed class of [TENxIO::TENxFile].
+#'
+#' @details Typically, the user will not create an object of this class directly
+#'   but rather use the [TENxVisium()] constructor function to create an object
+#'   of this class in the background.
+#'
+#' @importClassesFrom TENxIO TENxFile
+#' @importFrom methods new is
+#'
 #' @exportClass TENxGeoJSON
 .TENxGeoJSON <- setClass(
     Class = "TENxGeoJSON",
     contains = "TENxFile"
 )
 
+#' @rdname TENxGeoJSON-class
+#'
+#' @inheritParams TENxIO::TENxFile
+#'
+#' @return `TENxGeoJSON()`: An object of class [TENxGeoJSON]
+#'
 #' @export
 TENxGeoJSON <- function(resource) {
     if (!is(resource, "TENxFile"))
@@ -13,6 +31,14 @@ TENxGeoJSON <- function(resource) {
     )
 }
 
+#' @rdname TENxGeoJSON-class
+#'
+#' @inheritParams BiocIO::import
+#'
+#' @importFrom BiocBaseUtils checkInstalled
+#'
+#' @return import-method: An `sf` and `data.frame` with the GeoJSON data
+#'
 #' @exportMethod import
 setMethod("import", "TENxGeoJSON", function(con, format, text, ...) {
     checkInstalled("sf")
